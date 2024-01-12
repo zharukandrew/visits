8e289b416674ef2a7381c552f86150e8ae49147d
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2024-01-12 10:05:02 +0400

    hhhh

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let headers = document.querySelector(".headers");
 let photosNameTitle = document.querySelector(".photos__name-title");
+let stay = document.querySelector(".stay");
+let staysTitle = document.querySelector(".stays__title");
+let footer = document.querySelector(".footer");
 photosNameTitle.addEventListener("click",()=>{
   photosNameTitle.classList.toggle("phota__name-title")
 })
 headers.addEventListener("click",()=>{
   headers.classList.toggle("red")
+})
+stay.addEventListener("click",()=>{
+  stay.classList.toggle("phota__name-title")
+})
+staysTitle.addEventListener("click",()=>{
+  stay.classList.toggle("red")
 })
