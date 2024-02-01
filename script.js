c33376e594efded8e24af0fe5f40e5fbfb9e57b2
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2024-02-01 19:23:44 +0400

    Add new fails

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+import{lists}from'./texInner/textinner.js'
+//переменные
+let list = document.querySelector(".headers__logo");
 let headers = document.querySelector(".headers");
 let photosNameTitle = document.querySelector(".photos__name-title");
 let stay = document.querySelector(".stay");
-let staysTitle = document.querySelector(".stays__title");
-let footer = document.querySelector(".footer");
-photosNameTitle.addEventListener("click",()=>{
-  photosNameTitle.classList.toggle("phota__name-title")
-})
-headers.addEventListener("click",()=>{
+const staysTitle = document.querySelector(".stays__title");
+//обработчики событий
+function header(headers){
+  headers.addEventListener("click",()=>{
   headers.classList.toggle("red")
 })
-stay.addEventListener("click",()=>{
+}
+function photos(photosNameTitle){
+  photosNameTitle.addEventListener("click",()=>{
+      photosNameTitle.classList.toggle("phota__name-title")
+    })
+}
+function photaName(stay){
+  stay.addEventListener("click",()=>{
   stay.classList.toggle("phota__name-title")
 })
-staysTitle.addEventListener("click",()=>{
+}
+function staysTitler(staysTitle){
+  staysTitle.addEventListener("click",()=>{
   stay.classList.toggle("red")
 })
-//
+}
+
+
+
+
+//результат
+staysTitler(staysTitle)
+header(headers);
+photos(photosNameTitle);
+photaName(stay);
+lists(list);
---
+++ texInner/textinner.js
@@ -0,0 +1,10 @@
+
+ let list = document.querySelector(".headers__logo")
+function lists(list){
+    list.addEventListener("click",()=>{
+        list.innerHTML =`<p>gggg</p>`
+    })
+}
+
+export {lists}
+
